71b8a5ab6fd078135ee3f0513e5f5ff22e58d884
Lauryn-gitahi <lauryn.gitahi@gmail.com>
2020-07-26 18:04:47 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+// let fade= () => {
+//   const explore=document.querySelector(".kenya-showacase button");
+//   const home= document.querySelector(".kenya-showcase");
+//   const blog=document.querySelector(".blog");
+//
+//   // explore.addEventListener("click", () => {
+//     // home.classList.add("fadeOut");
+//     // blog.classList.add("fadeIn") {
+//
+//       }
+//     }
+//
+//   })
+// }
